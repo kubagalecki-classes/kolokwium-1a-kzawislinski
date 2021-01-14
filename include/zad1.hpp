75151cf7631b4d93650fed23f00f53b98d050fd5
kzawislinski <k.zawislinski@gmail.com>
2021-01-14 11:56:40 +0000

    Zad2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,11 +4,19 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
+class Makaron {
+public:
+    virtual ~Makaron() = default;
+
+    virtual double ileMaki(unsigned int P) const = 0;
+
+    static Makaron* gotujMakaron(const std::string& s);
+};
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
 
-class Tagliatelle
+class Tagliatelle : Makaron
 {
 
     public:
@@ -16,7 +24,7 @@ class Tagliatelle
 
     Tagliatelle(): L(0.5), W(0.5), R(0.5){}
 
-    double ileMaki(unsigned int P) const 
+    virtual double ileMaki(unsigned int P) const override 
     {
         return P * L * W * (1. - R) * C;
     }
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -6,4 +6,21 @@
 
 // tutaj klasa Penne
 
+class Penne : public Makaron {
+
+    virtual double ileMaki(unsigned int P) const override {
+        return static_cast<double>(P);
+    }
+};
+
 // tutaj definicja metody gotujMakaron
+
+Makaron* Makaron::gotujMakaron(const std::string& s)
+{
+    if (s.front() == s.back()) {
+        return new Tagliatelle(3.14, 0.42, 0.1);
+    }
+    else {
+        return new Penne();
+    }
+}

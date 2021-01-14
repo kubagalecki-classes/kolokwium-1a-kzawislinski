eae3e97763a167eba435ef48830964322d9d259c
kzawislinski <k.zawislinski@gmail.com>
2021-01-14 12:32:26 +0000

    zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,18 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+int obliczMake(const std::vector<Tagliatelle> v) {
+    int M = 0;
+    for (int i = v.size() - 1; i >= 0; i--) {
+        M += v[i].ileMaki(v.size() - i);
+    }
+    if (M > 100) {
+        throw 123;
+    }
+    else if (M > 50) {
+        throw 123.0;
+    }
+    else {
+        return M;
+    }
+}
